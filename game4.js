340ea3ca2ae88909803149b860c93b379716d434
choi jung yoon <lilly.choi@pikicast.com>
2016-03-23 16:54:11 +0900

    홀짝게

--- game4.js
+++ game4.js
@@ -1,14 +1,40 @@
 var randomLoc = Math.floor(Math.random()*10);
 var num = randomLoc % 2;
+var even = num >= 1; // 홀수
+var odd = num == 0; // 짝수
+console.log(randomLoc);
+console.log(num);
 
-function sum() {
-  if (num < 1) {
-    console.log("짝수")
+function evenodd() {
+  var result;
+  if (randomLoc == 0) {
+    console.log('정수');
+    result = '정수';
+  } else if (num >= 1) {
+    //console.log('홀수');
+    result = '홀수';
+  } else if (num == 0) {
+    //console.log('짝수');
+    result = '짝수';
   }
   else {
-    console.log("홀수")
+    console.log('형식에 맞지 않습니다.');
   }
+  return result;
 }
 
-console.log(randomLoc);
-console.log(sum());
+console.log(evenodd())
+
+function guess () {
+  var result = evenodd();
+  var useranswer = document.getElementById('number').value;
+  if (useranswer == '홀수' || useranswer == '짝수' ) {
+    if (result == useranswer) {
+      console.log('정답입니다.')
+    } else {
+      console.log('틀렸습니다.')
+    }
+  } else {
+    console.log('형식에 맞지 않습니다.')
+  }
+}
